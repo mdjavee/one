0bd406feb81c6f4306558627392654f5dc9c16d4
moinu <153938436+mdjavee@users.noreply.github.com>
2024-10-15 02:17:02 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1 className='xs:text-1xl mx-6 my-6'>Hello</h1>
+         <h1 className='xs:text-1xl xs:text-red-400 sm:text-cyan-300 smm:text-purple-400  mx-6 my-6 sm:text-2xl smm:text-3xl'>Hello</h1>
+   
     </div>
   )
 }
