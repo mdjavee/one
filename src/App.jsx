e2d73b8c09442cb82bb90ec9a8c49920d4ccff3c
moinu <mdjaveed8201@gmail.com>
2024-10-15 05:54:46 -0700

    third

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1 className='xs:text-1xl mx-6 my-6'>Hello</h1>
+      <h1 className='xs:text-2xl mx-6 my-6'>Hello</h1>
     </div>
   )
 }
