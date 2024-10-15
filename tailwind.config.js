2f66b8687e55ade5e879aa987828484ce6c297d5
moinu <mdjaveed8201@gmail.com>
2024-10-15 02:06:31 -0700

    second

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,jsx,js}"],
+  theme: {
+    extend: {},
+    screens:{
+      'xs':'320px',
+      'sm':'375px',
+      'smm':'425px',
+      'md':'768px',
+
+    }
+  },
+  plugins: [],
+}
+
