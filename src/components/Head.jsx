0d066461895c57757dddbd230599931f9c28b39e
moinu <mdjaveed8201@gmail.com>
2024-10-15 06:47:20 -0700

    g

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -7,7 +7,7 @@ const Head = () => {
         <Link to='/'><h1 className='xs:text-[40px] xs:mx-5 xs:my-6 title pb-4'>Myntra</h1>
         </Link><div>
             <nav>
-                <ul className='list-none flex xs:gap-3 xs:mx-4 xs:text-[18px]'>
+                <ul className='list-none flex xs:gap-3 xs:mx-4 xs:text-[19px]'>
                     <li><NavLink to='/Men'>Men</NavLink></li>
                     
                     <li><NavLink to='/Women'>Women</NavLink></li>
